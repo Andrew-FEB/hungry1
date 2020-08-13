6270c9517c93edb6fa2e3698ca267d8a0c1b6e47
andrew.gordon <andrew.gordon@sutdent.kuleuven.be>
2020-08-13 15:43:58 +0200

    updating .xml files

---
+++ src/main/java/web/LoginFilter.java
@@ -0,0 +1,41 @@
+package web;
+
+import ejb.UserSearchBean;
+import entities.User;
+
+import javax.ejb.EJB;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "LoginFilter")
+public class LoginFilter implements Filter {
+
+    @EJB
+    private UserSearchBean userSearch;
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        boolean userFound = userSearch.searchForUserWithEmail(email, password);
+        if (!userFound)
+        {
+            request.setAttribute("msg", "invalid");
+        }
+        else
+        {
+            request.setAttribute("permissions", userSearch.getPermissions(email));
+        }
+        chain.doFilter(req, resp);
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
