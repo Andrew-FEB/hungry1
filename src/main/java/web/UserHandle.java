5a3c39f4a0ddd9b306d1fe4c373648e4ea830bb1
andrew.gordon <andrew.gordon@sutdent.kuleuven.be>
2020-08-13 09:34:59 +0200

    Docker update mysql

--- src/main/java/web/UserHandle.java
+++ src/main/java/web/UserHandle.java
@@ -5,20 +5,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "SignIn", urlPatterns = "/signIn")
-public class SignIn extends HttpServlet {
+public class UserHandle extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Set response content type
-        response.setContentType("text/html");
-        String message = "Hello world";
-        // Actual logic goes here.
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
+
     }
 }
